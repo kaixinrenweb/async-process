9a32d3dc6753524bf7d89e3f97dd3341cf71c7e6
ksone <tls0401@126.com>
2019-04-13 00:04:04 +0800

    feat:add promise a+

--- promise/promise2.js
+++ promise/promise2.js
@@ -27,6 +27,7 @@ function Promise (exector){
     }
 }
 
+//兼容别人的promise
 function resolvePromise(promise2, x, resolve, reject){
     if(promise2 === x){
         throw new TypeError("循环引用");
@@ -34,27 +35,38 @@ function resolvePromise(promise2, x, resolve, reject){
     //如果x返回的是一个普通值，那直接就resolve
     if(x!=null && (typeof x==='object' || typeof x==='function')){ //可能就是一个promise
         //看当前的promise有木有then方法，有可能报错
+        let called;
         try {
-            let then = x.then;
+            let then = x.then; 
             if(typeof then==='function'){ //是一个promise
                 then.call(x, y=>{
-                    resolve(y);
+                    if(called) return;  
+                    called = true;
+                    //如果y依然是一个promise,递归解析
+                    resolvePromise(promise2, y, resolve, reject);
                 }, r=>{
+                    if(called) return; //防止调用失败，又调用成功
+                    called = true;
                     reject(r);
                 });
             }else{ //{then:{}}
                 resolve(x);
             }
         }catch(e){
+            if(called) return; //防止出错之后，继续调用成功的逻辑
+            called = true;
             reject(e);
         }
     }else{
-        resolve(x);
+        resolve(x);  //普通值
     }
 }
 
 //then
 Promise.prototype.then = function(onFulfilled, onRejcted){
+    //如果then的方法中没有传入成功或者失败的回调函数，就依次往下传
+    onFulfilled = typeof onFulfilled ==='function'? onFulfilled : val=>val;
+    onRejcted = typeof onRejcted=='function' ? onRejcted : err=>{throw err};
     let promise2 = new Promise((resolve,reject)=>{
         if(this.status=='fulfilled'){
             setTimeout(()=>{
@@ -100,6 +112,17 @@ Promise.prototype.then = function(onFulfilled, onRejcted){
         }
     });
     return promise2;
+}
+
+// npm install promises-aplus-tests -g
+// promises-aplus-tests promise2.js
+Promise.deferred = function(){
+    let dtd = {};
+    dtd.promise = new Promise((resolve, reject)=>{
+        dtd.resolve = resolve;
+        dtd.reject  = reject;
+    });
+    return dtd;
 }
 
 module.exports = Promise;
